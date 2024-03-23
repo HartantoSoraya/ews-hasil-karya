c385f6cc3f5207d3f8f5eff86651fc5fc42e37c6
Hugo YAW <hugoyaw22@gmail.com>
2024-03-23 16:46:58 +0700

    update

---
+++ database/seeders/EwsDeviceSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\EwsDevice;
+use App\Models\EwsDeviceAddress;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class EwsDeviceSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $count = 3;
+        for ($i = 1; $i <= $count; $i++) {
+            EwsDevice::factory()
+                ->has(EwsDeviceAddress::factory(), 'addresses')
+                ->create();
+        }
+    }
+}
