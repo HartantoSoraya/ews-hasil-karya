2072940afeda0b7d8bb885f4340017d76b5017f2
xrafffcode <yusfiwawan2@gmail.com>
2024-03-23 19:55:39 +0700

    Add API key validation middleware

--- routes/api.php
+++ routes/api.php
@@ -20,15 +20,17 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('ews-devices', [EwsDeviceController::class, 'index']);
-Route::get('ews-device-measurements', [EwsDeviceController::class, 'index']);
+Route::group(['middleware' => ['validate.api.key']], function () {
+    Route::get('ews-devices', [EwsDeviceController::class, 'index']);
+    Route::get('ews-device-measurements', [EwsDeviceController::class, 'index']);
 
-Route::post('ews-device', [EwsDeviceController::class, 'store']);
-Route::get('ews-device/{id}', [EwsDeviceController::class, 'show']);
-Route::post('ews-device/{id}', [EwsDeviceController::class, 'update']);
-Route::delete('ews-device/{id}', [EwsDeviceController::class, 'destroy']);
+    Route::post('ews-device', [EwsDeviceController::class, 'store']);
+    Route::get('ews-device/{id}', [EwsDeviceController::class, 'show']);
+    Route::post('ews-device/{id}', [EwsDeviceController::class, 'update']);
+    Route::delete('ews-device/{id}', [EwsDeviceController::class, 'destroy']);
 
-Route::post('ews-device-measurement', [EwsDeviceMeasurementController::class, 'store']);
-Route::get('ews-device-measurement/{id}', [EwsDeviceMeasurementController::class, 'show']);
-Route::post('ews-device-measurement/{id}', [EwsDeviceMeasurementController::class, 'update']);
-Route::delete('ews-device-measurement/{id}', [EwsDeviceMeasurementController::class, 'destroy']);
+    Route::post('ews-device-measurement', [EwsDeviceMeasurementController::class, 'store']);
+    Route::get('ews-device-measurement/{id}', [EwsDeviceMeasurementController::class, 'show']);
+    Route::post('ews-device-measurement/{id}', [EwsDeviceMeasurementController::class, 'update']);
+    Route::delete('ews-device-measurement/{id}', [EwsDeviceMeasurementController::class, 'destroy']);
+});
