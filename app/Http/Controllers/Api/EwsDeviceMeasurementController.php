7abd6ef42fcb8d57d6f232e7bf6d594ad2ddd6b5
xrafffcode <yusfiwawan2@gmail.com>
2024-03-24 03:20:18 +0700

    update

--- app/Http/Controllers/Api/EwsDeviceMeasurementController.php
+++ app/Http/Controllers/Api/EwsDeviceMeasurementController.php
@@ -37,46 +37,9 @@ class EwsDeviceMeasurementController extends Controller
         $request = $request->all();
 
         try {
-            $deviceCode = $request['device_code'];
-            $vibrationValue = $request['vibration_value'];
-            $dbValue = $request['db_value'];
-
             $request['ews_device_id'] = EwsDevice::where('code', '=', $request['device_code'])->first()->id;
 
             $ewsDeviceMeasurement = $this->EwsDeviceMeasurementRepository->create($request);
-
-            $messageBody = array(
-                "api_key" => "1672a326944cb9c6bfef5ffbc764b5c4988752f6",
-                "receiver" => "6285325483259", // Nomor penerima WhatsApp
-                "data" => array("message" => "Pengukuran EWS berhasil ditambahkan.\nDevice Code: $deviceCode\nVibration Value: $vibrationValue\nDB Value: $dbValue")
-            );
-
-            $curl = curl_init();
-            curl_setopt_array($curl, [
-                CURLOPT_URL => "https://whatsapp.hasilkarya.co.id/api/send-message",
-                CURLOPT_RETURNTRANSFER => true,
-                CURLOPT_ENCODING => "",
-                CURLOPT_MAXREDIRS => 10,
-                CURLOPT_TIMEOUT => 30,
-                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                CURLOPT_CUSTOMREQUEST => "POST",
-                CURLOPT_POSTFIELDS => json_encode($messageBody),
-                CURLOPT_HTTPHEADER => [
-                    "Accept: */*",
-                    "Content-Type: application/json",
-                ],
-            ]);
-
-            $response = curl_exec($curl);
-            $err = curl_error($curl);
-
-            curl_close($curl);
-
-            if ($err) {
-                Log::error("cURL Error #:" . $err);
-            } else {
-                Log::info("Response: " . $response);
-            }
 
             return ResponseHelper::jsonResponse(true, 'Data pengukuran ews berhasil ditambahkan.', new EwsDeviceMeasurementResource($ewsDeviceMeasurement), 201);
         } catch (\Exception $e) {
